95fc6829a86eab54f0d14d3546206da29ae30954
Balbina <balbinasantana@gmail.com>
2016-01-16 23:59:23 -0600

    days late

--- app/models/gondolla.rb
+++ app/models/gondolla.rb
@@ -1,2 +1,11 @@
 class Gondolla < ActiveRecord::Base
+
+  def days_late
+    if ((Time.now - created_at - 2.days).to_i / 1.day) < 0
+      0
+    else
+      ((Time.now - created_at - 2.days).to_i / 1.day)
+    end
+  end
+
 end
